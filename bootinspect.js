173912daaded9543642fb923d31335060730bde3
designmeltdown <mcneilp@GMAIL.COM>
2014-10-20 09:57:00 -0500

    Update bootinspect.js

--- bootinspect.js
+++ bootinspect.js
@@ -26,63 +26,34 @@
 $( document ).ready(function() {
 	$(".container, .container-fluid").addClass("bi-container-outline");
 	$(".container .row, .container-fluid .row").addClass("bi-row-outline");
-	biElement("col-xs-1","xs");
-	biElement("col-xs-2","xs");
-	biElement("col-xs-3","xs");
-	biElement("col-xs-4","xs");
-	biElement("col-xs-5","xs");
-	biElement("col-xs-6","xs");
-	biElement("col-xs-7","xs");
-	biElement("col-xs-8","xs");
-	biElement("col-xs-9","xs");
-	biElement("col-xs-10","xs");
-	biElement("col-xs-11","xs");
-	biElement("col-xs-12","xs");
 	
-	biElement("col-sm-1","sm");
-	biElement("col-sm-2","sm");
-	biElement("col-sm-3","sm");
-	biElement("col-sm-4","sm");
-	biElement("col-sm-5","sm");
-	biElement("col-sm-6","sm");
-	biElement("col-sm-7","sm");
-	biElement("col-sm-8","sm");
-	biElement("col-sm-9","sm");
-	biElement("col-sm-10","sm");
-	biElement("col-sm-11","sm");
-	biElement("col-sm-12","sm");
+	idBootstrapElement(12, "col")
+	idBootstrapElement(13, "push")
+	idBootstrapElement(13, "pull")
+	idBootstrapElement(13, "offset")
 	
-	biElement("col-md-1","md");
-	biElement("col-md-2","md");
-	biElement("col-md-3","md");
-	biElement("col-md-4","md");
-	biElement("col-md-5","md");
-	biElement("col-md-6","md");
-	biElement("col-md-7","md");
-	biElement("col-md-8","md");
-	biElement("col-md-9","md");
-	biElement("col-md-10","md");
-	biElement("col-md-11","md");
-	biElement("col-md-12","md");
-	
-	biElement("col-lg-1","lg");
-	biElement("col-lg-2","lg");
-	biElement("col-lg-3","lg");
-	biElement("col-lg-4","lg");
-	biElement("col-lg-5","lg");
-	biElement("col-lg-6","lg");
-	biElement("col-lg-7","lg");
-	biElement("col-lg-8","lg");
-	biElement("col-lg-9","lg");
-	biElement("col-lg-10","lg");
-	biElement("col-lg-11","lg");
-	biElement("col-lg-12","lg");
 });
 
 
-function biElement(target, showOn){
-	$(".container .row ." + target).prepend("<div class='bi bi-" + showOn + " bi-" + target + "'>" + target + "</div>").addClass("bi-" + target + "-outline");
-	$(".container-fluid .row ." + target).prepend("<div class='bi bi-" + showOn + " bi-" + target + "'>" + target + "</div>").addClass("bi-" + target + "-outline");
-	
+function biElement(target, showOn, type){
+	//alert(target+":"+showOn+":"+type); 
+	$(".container .row ." + target).prepend("<div class='bi bi-" + type + " bi-" + showOn + " bi-" + target + "'>" + target.substring(4,target.length) + "</div>").addClass("bi-" + target + "-outline");
+	$(".container-fluid .row ." + target).prepend("<div class='bi bi-" + showOn + " bi-" + target + "'>" + target.substring(4,target.length) + "</div>").addClass("bi-" + target + "-outline");	
 }
 
+function idBootstrapElement(count, type){
+	
+	
+	var sizes = ["xs","sm","md","lg"]
+	for(x=0; x<4 ; x++){
+		i = 1;
+		if(count==13){i=0};
+		for(; i<13; i++){
+			if(type=="col"){
+				biElement("col-"+sizes[x]+"-"+i,sizes[x],type);
+			}else{
+				biElement("col-"+sizes[x]+"-"+type+"-"+i,sizes[x],type);
+			}
+		}
+	}
+}	
